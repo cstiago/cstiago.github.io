8b0ea2e4d2d9c4aa38d885d340d538cdcaed7ece
Tiago Cunha <tiagocunha.me@gmail.com>
2025-10-16 13:47:41 -0300

    chore(macro): add optional complement to equation reference

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
 eqs = {}
 
-def handle_eq(eq: str) -> int:
+def handle_eq(eq: str, comp: str='') -> str:
+    ref = str()
     if eq not in eqs:
         eqs[eq] = len(eqs) + 1
-    return eqs[eq]
+        ref = str(eqs[eq])
+    if comp:
+        ref += comp
+    return ref
 
 def define_env(env):
 
     @env.macro
-    def label(eq: str):
-        ref = handle_eq(eq)
+    def label(eq: str, comp: str='') -> str:
+        ref = handle_eq(eq, comp)
         return f'\\label{{{ref}}}\\tag{{{ref}}}'
 
     @env.macro
-    def eqref(eq: str):
-        ref = handle_eq(eq)
+    def eqref(eq: str, comp: str='') -> str:
+        ref = handle_eq(eq, comp)
         return f'\\eqref{{{ref}}}'
